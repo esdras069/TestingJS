038be20be3e4abf2ee69d9833037559d7b799a6b
Esdras Ajanel <eajanel@tata.com.gt>
2023-02-28 16:38:38 -0600

    metodologias

--- DEMOS/src/06_person.test.js
+++ DEMOS/src/06_person.test.js
@@ -1,13 +1,22 @@
+/* AAA
+  Arrange / Given
+  Act / when
+  Asert / then
+*/
 const Person = require('./06_person');
 
 describe('Test for Person', () => {
   let person;
+  // Arrange
   beforeEach(() => {
     person = new Person('jsue', 30, 1.75);
   });
   test('Should return down', () => {
+    // Arrange
     person.weight = 30;
+    // Act
     const imc = person.calcIMC();
+    // Assert
     expect(imc).toBe('down');
   });
   test('Should return normal', () => {
